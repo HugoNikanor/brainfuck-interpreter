1c0538c5adc56be3723ed33afbd8ca01d0fbfe59
Hugo <hugo.hornquist@gmail.com>
2016-09-15 16:33:48 +0200

    proper command line handling in main

--- main.scm
+++ main.scm
@@ -5,7 +5,23 @@
 (use-modules (ice-9 rdelim))
 (load "parser.scm")
 
-;; TODO reenable access to debug output
+(define (parse-args args)
+  (define (inner rem parsed)
+    (cond
+      ((null? rem)
+       parsed)
+      ((= (length rem) 1)
+       (cons `(file . ,(car rem)) parsed))
+      ((equal? (car rem) "--debug")
+       (inner (cdr rem) (cons '(debug . #t) parsed)))))
+  (inner args '()))
+
+
 (define (main args)
-  (let ((file (open-input-file (cadr args))))
-    (parse-string (read-delimited (string #\eot) file))))
+  (let ((parsed-args (parse-args (cdr args))))
+    (let ((filename (assoc-ref parsed-args 'file))
+          (debug (assoc-ref parsed-args 'debug)))
+      (let ((file (open-input-file filename)))
+        (parse-string (read-delimited (string #\eot)
+                                      file)
+                      debug)))))
